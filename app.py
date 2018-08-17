26f396049620be6d4f637e89a6f205663714179b
Geert Depuydt <geert_depuydt@hotmail.com>
2018-08-17 16:48:53 +0200

    made the data table and graph interact with each other

--- app.py
+++ app.py
@@ -6,16 +6,28 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import dash_table_experiments as dt
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 
 app  = dash.Dash()
+app.config['suppress_callback_exceptions']=True
+
+MarkDownText1 = '''
+# Reporting to pharma prototype
+Please drag a demo csv file into the box below.
+'''
+
+MarkDownText2 = '''
+You can have a look at some Dash examples [here](https://dash.plot.ly/gallery).
+Enjoy!  
+'''
 
 app.layout = html.Div([
+    html.Div([dcc.Markdown(MarkDownText1)]),
     dcc.Upload(
         id='upload-data',
         children = html.Div([
-            'Drag and Drop or ',
-            html.A('Select csv data file')
+            'Drop or ',
+            html.A('Select csv Data File')
         ]),
         style = {
             'width': '100%',
@@ -30,7 +42,8 @@ app.layout = html.Div([
         multiple=True  
     ),
     html.Div(id='output-data-upload'),
-    html.Div(dt.DataTable(rows=[{}]), style={'display': 'none'})
+    html.Div(dt.DataTable(rows=[{}]), style={'display': 'none'}),
+    html.Div([dcc.Markdown(MarkDownText2)])
 ])
 
 def parse_contents(contents, filename, date):
@@ -50,9 +63,16 @@ def parse_contents(contents, filename, date):
         return html.Div(['There was an error processing this file. File type not supported?'])
     return html.Div([
         html.H1(filename),
-        dt.DataTable(rows=data.to_dict('records')),
+        dt.DataTable(
+            rows=data.to_dict('records'),
+            row_selectable = True,
+            filterable=True,
+            sortable=True,
+            selected_row_indices=[],
+            id='datatable-hospitals'
+        ),
         dcc.Graph(
-            id='basic-interactions',
+            id='datagraph-hospitals',
             figure={
                 'data': [
                     {
@@ -78,6 +98,43 @@ def update_output(content, name, date):
             parse_contents(c, n, d) for c, n, d in zip(content, name, date)
         ]
         return children
+
+@app.callback(
+    Output('datatable-hospitals', 'selected_row_indices'),
+    [Input('datagraph-hospitals', 'clickData')],
+    [State('datatable-hospitals', 'selected_row_indices')])
+def update_selected_row_indices(clickData, selected_row_indices):
+    if clickData:
+        for point in clickData['points']:
+            if point['pointNumber'] in selected_row_indices:
+                selected_row_indices.remove(point['pointNumber'])
+            else:
+                selected_row_indices.append(point['pointNumber'])
+    return selected_row_indices
+
+@app.callback(
+    Output('datagraph-hospitals', 'figure'),
+    [Input('datatable-hospitals', 'rows'),
+     Input('datatable-hospitals', 'selected_row_indices')])
+def update_figure(rows, selected_row_indices):
+    dff = pd.DataFrame(rows)
+    
+    marker = {'color': ['#0074D9']*len(dff), 'size':12}
+    for i in (selected_row_indices or []):
+        marker['color'][i] = '#FF851B'
+    fig={
+            'data': [{
+                'x': dff.iloc[:,0],
+                'y': dff.iloc[:,1],
+                'name': 'Trace 1',
+                'mode': 'markers',
+                'marker': marker
+
+            }]
+    }
+        
+    return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=False)
